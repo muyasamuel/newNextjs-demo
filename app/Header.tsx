938fa4a446fd539d6afd8e3315fbb3485e9645be
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-06 12:01:26 +0300

    Add a Todos page through app

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react'
 
 function Header() {
   return (
-    <header className='p-5 bg-red-500'>
+    <header className=' flex  items-center  space-x-6 p-5 bg-red-500'>
         <h1 className='text-white'>I am a Header</h1>
+        
+        <Link href='/' className='p-2 bg-slate-100 rounded-md'> Home </Link>
     </header>
   )
 }
