8bc0d299d1c3acec5986d78b384c9ba641fe7f36
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-06 11:45:12 +0300

    first commit

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='p-5 bg-red-500'>
+        <h1 className='text-white'>I am a Header</h1>
+    </header>
+  )
+}
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-blue-500'>I am the HomePage</div>
+  )
+}
+
+export default Home;
